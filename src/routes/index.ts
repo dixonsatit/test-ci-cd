36e67176d5f22e66c394cb7585e128d629285acb
Sathit Seethaphon <dixonsatit@gmail.com>
2018-08-26 12:22:31 +0700

    add text ci

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/',(req,res,next) => {
-  res.render('index', {title: 'Express Update gitlab'});
+  res.render('index', {title: 'Express Update gitlab CI'});
 });
 
 export default router;
