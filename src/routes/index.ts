7fdc7b00b39b358fb49760df0c2744192897df7a
Sathit Seethaphon <dixonsatit@gmail.com>
2018-08-26 11:41:21 +0700

    update text

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/',(req,res,next) => {
-  res.render('index', {title: 'Express นะจ่ะ นะๆ'});
+  res.render('index', {title: 'Express Update'});
 });
 
 export default router;
